4cf07ea4369648b0b8e07e0dc1a9710c585927a4
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-08-01 12:25:29 +0400

    initial commit

---
+++ src/assets/logo.ts
@@ -0,0 +1,15 @@
+import logoGaddgets from './logo-gadgets-now.png'
+import logoJakarta from './logo-jakarta-post.png'
+import logoTech from './logo-tech-radar.png'
+import logoGurdian from './logo-the-guardian.png'
+import logoVerge from './logo-the-verge.png'
+import logo from './logo.png'
+
+export {
+    logo,
+    logoGaddgets,
+    logoGurdian,
+    logoJakarta,
+    logoTech,
+    logoVerge
+}
